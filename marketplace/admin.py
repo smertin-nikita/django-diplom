0aaaf9f1938795d5664190365538e73635c111f2
Smertin Nikita Job <smertin-nikita@mail.ru>
2021-05-24 14:11:06 +0500

    Added todo: set amount when changed products

--- marketplace/admin.py
+++ marketplace/admin.py
@@ -1,12 +1,18 @@
 from django.contrib import admin
 
-from marketplace.models import Product, Collection, Order, Review
+from marketplace.models import Product, Collection, Order, Review, ProductOrder
 
 admin.site.register(Product)
 admin.site.register(Collection)
 admin.site.register(Review)
 
 
+class ProductOrderInline(admin.TabularInline):
+    model = ProductOrder
+
+# todo разобраться на каком этапе считать сумму заказа
 @admin.register(Order)
 class OrderAdmin(admin.ModelAdmin):
     ordering = ['created_at']
+    readonly_fields = ('amount',)
+    inlines = [ProductOrderInline]
