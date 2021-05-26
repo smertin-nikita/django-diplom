651683fa90d9714e48df62c704b64ddbc7cccc7e
Smertin Nikita Job <smertin-nikita@mail.ru>
2021-05-26 14:57:08 +0500

    Added product routing

--- marketplace/urls.py
+++ marketplace/urls.py
@@ -1,8 +1,9 @@
 from rest_framework.routers import DefaultRouter
 
-from marketplace.views import ReviewViewSet
+from marketplace.views import ReviewViewSet, ProductViewSet
 
 router = DefaultRouter()
 router.register('review', ReviewViewSet, basename='review')
+router.register('product', ProductViewSet, basename='product')
 
 urlpatterns = router.urls
