be06e48dc77d4e8633bf1cf63d53b623f0a1c3c2
Smertin Nikita <smertin-nikita@mail.ru>
2021-05-26 21:25:47 +0500

    Added django-filter

--- website/settings.py
+++ website/settings.py
@@ -79,6 +79,9 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.TokenAuthentication',
     ],
+    'DEFAULT_FILTER_BACKENDS': [
+        'django_filters.rest_framework.DjangoFilterBackend'
+    ]
 }
 
 WSGI_APPLICATION = 'website.wsgi.application'
