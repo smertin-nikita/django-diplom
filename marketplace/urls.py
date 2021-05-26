a4094c8defcd31b1f9f01bbc43de983b0bf0db3c
Smertin Nikita Job <smertin-nikita@mail.ru>
2021-05-26 15:17:34 +0500

    Added collection and order routing

--- marketplace/urls.py
+++ marketplace/urls.py
@@ -1,9 +1,12 @@
 from rest_framework.routers import DefaultRouter
 
-from marketplace.views import ReviewViewSet, ProductViewSet
+from marketplace.views import ReviewViewSet, ProductViewSet, CollectionViewSet, OrderViewSet
 
 router = DefaultRouter()
 router.register('review', ReviewViewSet, basename='review')
 router.register('product', ProductViewSet, basename='product')
+router.register('order', OrderViewSet, basename='order')
+router.register('collection', CollectionViewSet, basename='collection')
+
 
 urlpatterns = router.urls
