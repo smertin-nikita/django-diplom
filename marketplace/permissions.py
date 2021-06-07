cfea03b82c58282c76d45b6705d8ef0fea059a02
Smertin Nikita Job <smertin-nikita@mail.ru>
2021-06-07 15:11:59 +0500

    fix and added new permission

--- marketplace/permissions.py
+++ marketplace/permissions.py
@@ -1,6 +1,19 @@
 from rest_framework.permissions import BasePermission, SAFE_METHODS, IsAdminUser
 
 
+class IsAdminUserOrReadOnly(IsAdminUser):
+    """
+    Allows access only to admin users.
+    """
+
+    def has_permission(self, request, view):
+        # Read permissions are allowed to any request,
+        # so we'll always allow GET, HEAD or OPTIONS requests.
+        if request.method in SAFE_METHODS:
+            return True
+        return super().has_permission(request, view)
+
+
 class IsOwnerOrReadOnly(BasePermission):
     """
     Object-level permission to only allow owners of an object to edit it.
@@ -27,15 +40,14 @@ class IsOwnerUser(BasePermission):
         return obj.creator == request.user
 
 
-class OnlyAdminEditToOrderStatus(BasePermission):
+class OnlyAdminEditToOrderStatus(IsAdminUser):
     """
     Object-level permission to allows only admins to edit status of order.
     """
 
     def has_object_permission(self, request, view, obj):
-        # Todo Посмотреть откуда брать статус в
         # Instance must have an attribute named `status`.
         if bool(request.data.get('status')):
-            return IsAdminUser.has_permission(self, request, view)
+            return super().has_permission(request, view)
         return True
 
