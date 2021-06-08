eed839946c4cb1f4059849f550b927b6d366c518
Smertin Nikita Job <smertin-nikita@mail.ru>
2021-06-08 10:35:09 +0500

    User factory

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,4 +1,5 @@
 import pytest
+from django.contrib.auth import get_user_model
 from model_bakery import baker
 from rest_framework.test import APIClient
 
@@ -13,5 +14,13 @@ def api_client():
 def product_factory():
     def func(**kwargs):
         return baker.make('product', **kwargs)
+
+    return func
+
+
+@pytest.fixture
+def user_factory():
+    def func(**kwargs):
+        return baker.make(get_user_model(), **kwargs)
 
     return func
