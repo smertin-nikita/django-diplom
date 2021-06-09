27fbd62e894d8df6392d0eb7b155cb8ae7762131
Smertin Nikita Job <smertin-nikita@mail.ru>
2021-06-09 15:37:18 +0500

    added IsOwnerOrAdminFilterBackend

--- marketplace/filters.py
+++ marketplace/filters.py
@@ -1,5 +1,5 @@
 from django_filters import rest_framework as filters, DateTimeFromToRangeFilter
-from rest_framework.filters import SearchFilter
+from rest_framework.filters import SearchFilter, BaseFilterBackend
 
 from marketplace.models import Product, Review, Order
 
@@ -40,3 +40,14 @@ class OrderFilter(filters.FilterSet):
     class Meta:
         model = Order
         fields = ['status', 'created_at', 'updated_at', 'amount']
+
+
+class IsOwnerOrAdminFilterBackend(BaseFilterBackend):
+    """
+    Filter that only allows users to see their own objects and allows admins to see all objects.
+    """
+    def filter_queryset(self, request, queryset, view):
+        if request.user.is_staff:
+            return queryset
+        else:
+            return queryset.filter(creator=request.user)
